3a00e7d640a6b5e9a49a92847ce05d0bba7d08f6
bobcoi03 <elongatedmusk6969@gmail.com>
2023-01-26 15:20:43 +0000

    Added google analytics tags to _app.js

--- personalwebsite/src/pages/_app.js
+++ personalwebsite/src/pages/_app.js
@@ -1,5 +1,20 @@
 import '@/styles/globals.css'
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Z32KHW8D9X"></Script>
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Z32KHW8D9X');
+        `}
+      </Script>
+      <Component {...pageProps} />      
+    </>
+  )
 }
